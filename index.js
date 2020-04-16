47b6d6cbef411678e70aac009a89f4cdfb70450b
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-04-16 01:17:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const fareDoubler = createFareMultiplier(2)
 
 const fareTripler = createFareMultiplier(3)
 
-const selectDifferentDrivers = function(drivers, ){
+const selectDifferentDrivers = function(drivers, returnFirstTwoDrivers || returnLastTwoDrivers){
   
 }
 
